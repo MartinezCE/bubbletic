ae5857447a9dfbf8172a0ef0564aeff2a673dc44
MaruGargiulo <mariaeugenia@agropro.ag>
2023-03-24 17:56:58 -0300

    initial landing

---
+++ app/page.jsx
@@ -0,0 +1,24 @@
+import AboutSectionOne from "@/components/About/AboutSectionOne";
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import Brands from "@/components/Brands";
+import Contact from "@/components/Contact";
+import Features from "@/components/Features";
+import Hero from "@/components/Hero";
+import Pricing from "@/components/Pricing";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Brands />
+      <AboutSectionOne />
+      <AboutSectionTwo />
+      <Pricing />
+      <Contact />
+    </>
+  );
+}
